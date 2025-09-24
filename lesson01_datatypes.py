b72234e4414972ff8e6a66fef3188155cc0f263b
Aishwarya C <aishwaryachandalada@gmail.com>
2025-09-24 16:18:01 +0000

    creates data types lesson

---
+++ lesson01_datatypes.py
@@ -0,0 +1,39 @@
+# This is a comment 
+#DATA TYPES
+
+# 1. STRINGS: 
+print("Hello World")
+print(" ")
+print("once upon a time \nlong ago there was a hare \na very pretty hare")
+print("Name: \tAishwarya \nAge: \t14 \nGrade: \t9 ")
+
+# INTEGERS:
+
+print(42)
+print(-23)
+print(5 + 5 - 11)
+
+# FLOAT: 
+
+print(4.0)
+print(2.0 + 4.5)
+print(2.0 + 4)
+print(3.3 + 5.3)
+
+# BOOLEAN:
+
+print(True) # 1
+print(False) # 0
+print(True + True)
+print(1 == 1 + 0)
+print(1 != 2)
+print(1 != 2 + False)
+print("Hello" == "Hello")
+print("Hot" != "Cold")
+
+#How do I know what data type I'm working with?
+
+print(type("To see this message, wrap me in a print statement"))
+print(type(9))
+print(type(4.2))
+print(type(True == True + False))
