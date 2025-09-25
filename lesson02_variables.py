f71cb709f08a6db802699b4e61bec3a0e7fe4ea2
Aishwarya C <aishwaryachandalada@gmail.com>
2025-09-25 19:27:51 +0000

    lesson02 variables

---
+++ lesson02_variables.py
@@ -0,0 +1,62 @@
+#CONCEPTS VARIABLES (Holds a value in pysical storage)
+
+french = "fries"
+
+password = "08082011ac"
+email = "iheartglitter@gmail.com"
+print("Password:", password)
+print("Email:", email)
+
+temperature = 72.5
+print(type(temperature))
+
+enabled = False
+is_complete = False 
+print(is_complete)
+
+enabled = True
+print(enabled)
+
+#Variable name must be meaningful 
+# For math problems: 
+
+x = 3.14
+y = 7
+print(x + y)
+
+#Variables are flexable, you create or update another variable like so: 
+
+count = 10 
+print(count)
+count_down = count - 1
+print(count_down)
+count = count_down
+print(count)
+count_down = count -1
+print(count_down)
+count = count_down
+print(count)
+
+#Challenge 1: rename variables 
+# Change the variable names x, y, z belowe to more descriptive names.
+
+Name = "Radia Perlman"
+Interger = 34
+Job = "Networking Engineer"
+
+count = 10 
+count_up = count +5
+count = count_up
+print(count)
+
+x = 4
+y = "hello"
+swap = y
+y = x
+x = swap
+print("x = ", x)
+print("y = ", y)
+
+is_raining = False
+is_raining = True 
+print("is it raining?: ", is_raining)
