558a30c7ff2202e6d197d016361de4f20dc683ce
Aishwarya C <aishwaryachandalada@gmail.com>
2025-09-26 18:00:29 +0000

    basic math operators

---
+++ lesson03_math.py
@@ -0,0 +1,45 @@
+#KEY CONCEPTS: math operators: +, -, *, /, //, %, **
+
+add = 7 + 2
+print("Sum: ", add)
+
+subtract = 7 - 2
+print("Difference: ", subtract)
+
+multiply = 7 * 2
+print("Product: ", multiply)
+
+# One slash will always be a float
+
+Float_divide = 7 / 2
+print("Quotient: ", Float_divide)
+
+# Two slashes will always be an integer 
+
+integer_divide = 7 // 2
+print("Integer Quotient: ", integer_divide)
+
+modulus = 7 % 2
+print("modulus: ", modulus)
+
+exponent = 7 ** 2
+print("Exponent: ", exponent)
+
+print(2 + 2 != 10 - 6)
+print(2 + 2 == 10 - 6)
+
+result1 = (2 + 3) * 4
+print("Result 1: ", result1)
+
+result2 = 2 ** 3 * 4
+print("Result 2: ", result2)
+
+result3 = 20 / 5 * 2
+print("Result 3: ", result3)
+
+result4 = 10 - 2 + 3
+print("Result 4: ", result4)
+
+result5 = 5 + 2 ** 3 * (4-1)
+print("Result 5: ", result5 )
+
