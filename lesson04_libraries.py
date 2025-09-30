3056e41c2fc9ca13f4c79c0700778b8fe2b12d97
Aishwarya C <aishwaryachandalada@gmail.com>
2025-09-30 16:27:15 +0000

    random number library

--- lesson04_libraries.py
+++ lesson04_libraries.py
@@ -8,3 +8,19 @@ print("Round down to an integer: ", math.floor(4.2))
 print("Round up to an integer: ", math.ceil(4.2))
 print("Power: ", math.pow(2, 5))
 print("Pi: ", math.pi)
+
+
+#RANDOM LIBRARY 
+# Pseudorandom Number Generator (PRNG) (Pseudo = Fake)
+#True Random Generator 
+
+print("\n--- Random Library ---\n")
+
+seed = 65936
+result_1 = seed * seed 
+print("Result 1: ", result_1)
+result_2 = result_1 * 15
+print("Result 2: ", result_2)
+result_3 = result_2 // 6
+print("Result 3: ", result_3)
+
