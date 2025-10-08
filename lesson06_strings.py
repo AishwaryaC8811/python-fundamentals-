be7d3d2e70157b058e4c98304663dbbb26c7371c
Aishwarya C <aishwaryachandalada@gmail.com>
2025-10-08 16:53:01 +0000

    Update strings

--- lesson06_strings.py
+++ lesson06_strings.py
@@ -31,7 +31,31 @@ country = "Bahamas"
 length_of_word = len(country)
 print(length_of_word)
 
-phrase = "SpOnGEbOB"
-print("\nUppercase:", phrase.upper())
-print("\nLowercase:", phrase.lower())
-print("Capitalized:", phrase.capitalize())
+character = "SpOnGEbOB sQuaREPanTs"
+print("\nUppercase:", character.upper())
+print("\nLowercase:", character.lower())
+print("Capitalized:", character.capitalize())
+print("Title Format:", character.title())
+
+print()
+
+#Find and replace text 
+sentence = "I'm a goofy goober"
+new_sentence = sentence.replace("I'm", "You're")
+print(sentence)
+print(new_sentence)
+
+# FORMATTED STRINGS IN PYTHON
+# f-strings allow variables and expressions inside strings
+
+print("\n--- Formatted Strings ---")
+
+name = "Bob" 
+age = 70
+city = "Miami"
+
+print(f"Hello, my name is {name}. I am {age} years old and live in {city}.")
+
+# f-strings can do math and function calls inside {}
+
+print(f"Next year, I'll be {age + 1}, My name in uppercase is {name.upper()}.")
