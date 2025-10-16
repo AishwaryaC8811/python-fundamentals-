e6f8d4679b43dd7173387b6717ce477cf441f20a
Aishwarya C <aishwaryachandalada@gmail.com>
2025-10-16 20:32:38 +0000

    lists w/ challenges

--- lesson08_lists.py
+++ lesson08_lists.py
@@ -5,6 +5,11 @@
 print()
 print("--- Lists in Python ---")
 
+#initialize a list with brackets:
+empty_list = []
+empty_list.append("Thing")
+print(empty_list)
+
 animals = ["donkey", "pangolin", "blobfish"]
 numbers = [2, 4, 6, 8, 10]
 mixed = ["piffle", 42, True, 9.99]
@@ -25,6 +30,10 @@ print("--- Modifying Lists ---")
 
 animals[0] = "babirusa"
 print(animals)
+
+animal_to_replace = animals.index("pangolin")
+print(animal_to_replace)
+animals[animal_to_replace] = "seamonkey"
 
 # .append() - To add it to the end of a list
 
@@ -48,4 +57,75 @@ print("Remaining animals:", animals)
 print()
 print("--- Useful List Functions ---")
 
+nums = [3, 7, 1, 9, 4, 2, 5, 8, 6, 0]
+print("Original Numbers:", nums)
+print("Length of the list:", len(nums))
+print("Min:", min(nums))
+print("Max:", max(nums))
+print("Sum:", sum(nums))
 
+nums.sort()
+print(nums)
+animals.sort()
+print(animals)
+
+nums.reverse()
+print(nums)
+
+print()
+print("--- Checking Membership ---")
+
+if "cat" in animals:
+    print("Cat is in the list!") 
+else:
+    print("Cat is not in the list")
+
+print()
+print("--- Copying Lists ---")
+
+original_list = [1, 2, 3]
+copied_list = original_list.copy()
+copied_list.append(4)
+print(original_list)
+print(copied_list)
+
+print()
+print("--- Nested Lists/The Matrix ---")
+
+matrix = [  
+    [1,2,3], 
+    [4,5,6], 
+    [7,8,9]  
+    ] 
+
+print(matrix[2][2])
+
+### **Challenge 1: Integer Swap**
+
+# Store this list in a variable: [ 1, 2, 3, 4, 5, 6 ] 
+# Ask the user to enter a new integer.
+# Replace the **third integer** in the original list with the user’s input, and then print the updated list.
+# *Hint: use indexing and assignment.*
+
+challenge_list = [ 1, 2, 3, 4, 5, 6 ] 
+new_integer = int(input("Enter an integer:"))
+replaced_integer = challenge_list.index(3)
+challenge_list[replaced_integer] = new_integer
+print(challenge_list)
+print()
+print()
+### **Challenge 2: Shopping List Manager**
+
+# Initialize an empty list called `shopping`.
+# Add three items of your choice using `.append()`.
+# Then insert one extra item at the second position 
+# Remove one item of your choice.
+# Finally, print the final shopping list.
+
+shopping = []
+shopping.append("goldfish")
+shopping.append("crackers")
+shopping.append("soap")
+shopping.insert(1, 'cheese')
+shopping.remove("soap")
+print(shopping)
